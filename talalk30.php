cf966cf008e4391a780b7db479a122444b9a3528
Levi <code@blue-l.de>
2015-04-23 00:43:46 +0200

    Ok

--- talalk30.php
+++ talalk30.php
@@ -10,14 +10,14 @@ include("homemenu.php");
 <h4>Augusztus 13 csütörtök:</h4> 
 Délután  5-6-7 óra körül temetö látogatás,
 
-<h4>Augusztus 14 pÃ©ntek:</h4> 
+<h4>Augusztus 14 péntek:</h4> 
 Délelött 10-11 osztályfönöki óra,délután indulás Torockora.<br />
 <b><a href="https://www.google.de/maps/place/46%C2%B028'18.8%22N+23%C2%B034'58.8%22E/@46.471876,23.58299,398m/data=!3m2!1e3!4b1!4m2!3m1!1s0x0:0x0"  taget="blank">Panzió</a></b> 
 Délután 7-kor vacsora, éjjelre is lesz valami étel.<br />
  
 <h4>Augusztus 15 szombat:</h4> 
-Reggel 10 óra körül indulás a 38,6km-re levö Szolcsvai-búvópatakhoz.<a href="https://www.google.de/maps/dir/Sub+Piatr%C4%83,+Rum%C3%A4nien/46.4718537,23.5829783/@46.3986645,23.5129027,19938m/data=!3m1!1e3!4m9!4m8!1m5!1m1!1s0x474948e358644b8f:0x78616929543288dc!2m2!1d23.4607753!2d46.3833121!1m0!3e0?hl=hu"><b>ÃºtleÃ­rÃ¡s</b></a><br />
-Itt hagyjuk az autókat, elmegyünk a barlangszájig (<a href="https://ssl.panoramio.com/photo/98918629"><b>itt jön ki a patak a sziklából</b></a>), egy lengöhídon át, majd felsétálunk az erdöben, kb. 1,6 km, kiérünk egy nyeregbe, innen ereszkedünk le a <a href="https://ssl.panoramio.com/photo/42505289"><b>Dilbina-vÃ­zesÃ©shez</b></a>, itt gyül össze 3 patak vize, és folyik be a szikla alá. 
+Reggel 10 óra körül indulás a 38,6km-re levö Szolcsvai-búvópatakhoz.<a href="https://www.google.de/maps/dir/Sub+Piatr%C4%83,+Rum%C3%A4nien/46.4718537,23.5829783/@46.3986645,23.5129027,19938m/data=!3m1!1e3!4m9!4m8!1m5!1m1!1s0x474948e358644b8f:0x78616929543288dc!2m2!1d23.4607753!2d46.3833121!1m0!3e0?hl=hu"><b>Útleírás</b></a><br />
+Itt hagyjuk az autókat, elmegyünk a barlangszájig (<a href="https://ssl.panoramio.com/photo/98918629"><b>itt jön ki a patak a sziklából</b></a>), egy lengöhídon át, majd felsétálunk az erdöben, kb. 1,6 km, kiérünk egy nyeregbe, innen ereszkedünk le a <a href="https://ssl.panoramio.com/photo/42505289"><b>Dilbina-vízeséshez</b></a>, itt gyül össze 3 patak vize, és folyik be a szikla alá. 
 Könnyü túra gyönörü kilátással, nem szabad kihagyni! Persze, nem kötelezö, csak ajánlott.<br />
 Visszafelé az Aranyos partján, megállunk valahol, tüzet rakunk, szalonnát sütünk. Délben nincs ebéd, majd este közös gulyás lesz, mint a múltkor.
 Este bulizás, jókedv, éneklés, tánc, beszélgetés.
