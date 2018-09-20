ca3023b41a23e173bdeb679958fd1b893aa7395b
Levi XPS <code@blue-l.de>
2018-09-20 17:22:54 +0200

    Nametest

---
+++ tools/test/NameSyntax.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Levi
+ * Date: 20.09.2018
+ * Time: 16:07
+ */
+
+namespace maierlabs\lpfw;
+
+use nameSyntax;
+
+class NameSyntaxCheck extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MySqlDb
+     */
+    private $db ;
+
+    /**
+     * @var array
+     */
+    private $firstName = array();
+    private $lastName = array();
+
+    public function setup()
+    {
+
+        $include = array('mysql.class.php');
+        $argv=$_SERVER["argv"];
+        $p=pathinfo($argv[sizeof($argv)-1]);
+        foreach ($include as $item) {
+            if (file_exists($p["dirname"].'/'.$item)){
+                include_once $p["dirname"].'/'.$item;
+            } else if (file_exists($p["dirname"].'/brassai/tools/'.$item)){
+                include_once $p["dirname"].'/brassai/tools/'.$item;
+            } else if (file_exists($p["dirname"].'/tools/'.$item)){
+                include_once $p["dirname"].'/tools/'.$item;
+            } else if (file_exists($p["dirname"].'/../'.$item)){
+                include_once $p["dirname"].'/../'.$item;
+            } else {
+                throw (new Exception("Inludefile not found"));
+            }
+        }
+
+
+        $this->db = new MySqlDb("localhost","db652851844","root","root");
+
+    }
+
+    public function testNames() {
+        $this->assertTrue($this->db->query("select firstname,lastname,id from person"));
+        while ($row = $this->db->fetchRow()) {
+            $this->firstName = $this->addName($this->firstName,$row["firstname"]);
+            $this->lastName = $this->addName($this->lastName,$row["lastname"]);
+        }
+        arsort($this->firstName,SORT_NUMERIC);
+        arsort($this->lastName,SORT_NUMERIC);
+        echo(sizeof($this->firstName));
+    }
+
+    /**
+     * @param string $name
+     */
+    private function addName($nameArray,$name) {
+        $name = str_replace("-"," ",$name);
+        $name = str_replace("."," ",$name);
+        $name = str_replace("("," ",$name);
+        $name = str_replace(")"," ",$name);
+        $names = explode(" ",$name);
+        foreach ($names as $n) {
+            $n = trim($n);
+            if (strlen($n)>2) {
+                if (!array_key_exists($n, $nameArray)) {
+                    $nameArray[$n] = 1;
+                } else {
+                    $nameArray[$n] = $nameArray[$n] + 1;
+                }
+            }
+        }
+        return $nameArray;
+    }
+}
