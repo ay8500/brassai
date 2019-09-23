2210bf4adfa24b764da11506ca12633ca92aaed7
Levente <levente@blue-l.de>
2019-09-24 00:39:11 +0200

    Bugfix IP display for non admin user

--- displayOpinion.inc.php
+++ displayOpinion.inc.php
@@ -219,7 +219,7 @@ function getLoggedInUserIdOrNull() {
                 var text = '';
                 data.forEach(function(e) {
                     text +='<div><div class=\"uname\">'+e.name;
-                    text +='<span title=\"' +e.ip+  '\" onclick=\"showip(\''+e.ip+ '\')\"> IP</span>';
+                    text +='<span title=\"' +e.ip+  '\" onclick=\"showip(\''+e.ip+ '\')\"> '+(e.ip==''?'':' show IP')+'</span>';
                     text +='<span style=\"float:right\">'+e.date;
                     if (e.myopinion) {
                         text +=' <span title=\"kitöröl\" class=\"oglyph glyphicon glyphicon-remove\" onclick=\"deleteOpinion('+e.id+')\"></span>';
